6825d7f94527bfe126e3bb3cae9eec5cf4d510ce
lunstia <136163666+lunstia@users.noreply.github.com>
2023-07-31 16:02:00 -0400

    Production

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi! I am a default message, feel free to chat here!",
+    user: "lunstia",
+    added: new Date()
+  },
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Mini Messageboard", messages: messages })
 });
+
+router.post('/new', function(req, res, next) {
+  messages.unshift({text: req.body.messageText, user: req.body.messageUser, added: new Date()}); 
+  res.redirect('/')
+})
 
 module.exports = router;
